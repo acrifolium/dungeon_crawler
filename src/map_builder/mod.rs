7eb3d49265693a6073080efbde29187347060154
acrifolium <olivier.azevedo@gmail.com>
2023-04-13 13:25:44 +0200

    Add themes forest

--- src/map_builder/mod.rs
+++ src/map_builder/mod.rs
@@ -3,13 +3,19 @@ mod drunkard;
 mod empty;
 mod prefab;
 mod rooms;
+mod themes;
 
 use crate::prelude::*;
 use automata::CellularAutomataArchitect;
 use drunkard::DrunkardsWalkArchitect;
-use empty::EmptyArchitect;
 use prefab::apply_prefab;
 use rooms::RoomsArchitect;
+use themes::DungeonTheme;
+use themes::ForestTheme;
+
+pub trait MapTheme: Sync + Send {
+    fn tile_to_render(&self, tile_type: TileType) -> FontCharType;
+}
 
 trait MapArchitect {
     fn new(&mut self, rng: &mut RandomNumberGenerator) -> MapBuilder;
@@ -23,6 +29,7 @@ pub struct MapBuilder {
     pub monster_spawns: Vec<Point>,
     pub player_start: Point,
     pub amulet_start: Point,
+    pub theme: Box<dyn MapTheme>,
 }
 
 impl MapBuilder {
@@ -34,6 +41,10 @@ impl MapBuilder {
         };
         let mut mb = architect.new(rng);
         apply_prefab(&mut mb, rng);
+        mb.theme = match rng.range(0, 2) {
+            0 => DungeonTheme::new(),
+            _ => ForestTheme::new(),
+        };
         mb
     }
 
